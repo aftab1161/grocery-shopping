042f4aeae9f81cce80a24dba945401fd6eefd08e
Aftab116 <aftabahmed116@gmail.com>
2020-05-11 04:09:48 +0530

    Lazy loading modules created. Folder structures made.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'seller', loadChildren: () => import('./modules/seller/seller.module').then(m => m.SellerModule) },
+  { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FirebaseModule} from './core/modules/firebase/firebase.module';
 
+
 @NgModule({
   declarations: [
     AppComponent
